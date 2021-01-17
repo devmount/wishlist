82e664d6224e86c285898ee819606217175a399f
Andreas Müller <mail@devmount.de>
2021-01-17 13:24:00 +0100

    ➕ add and init supabase

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="list">
+    <div class="item" v-for="(i, n) in items" :key="i.id">
+      <h2>#{{ n+1 }} {{ i.title }}</h2>
+      <p>{{ i.description }}</p>
+      <ul>
+        <li v-for="l in i.links">{{ l }}</li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
+// import external dependencies
+import { createClient } from '@supabase/supabase-js'
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxMDg4MjU1MiwiZXhwIjoxOTI2NDU4NTUyfQ.FLgpeb22wLfHaZl6bQD6ztDG5UiTPTC9ympeN4bq0bc'
+const SUPABASE_URL = "https://ttohmduvoxaknazzfyre.supabase.co"
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
+
+// import internal components
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data: () => ({
+    items: []
+  }),
+  async created () {
+    // subscribe to all items changes to prived realtime experience
+    supabase.from('items').on('*', payload => {  console.log('Change received!', payload)}).subscribe()
+    // initially get all existing items
+    this.items = await this.getItems()
+  },
+  methods: {
+    // retrieve list of items
+    async getItems () {
+      let { data: items, error } = await supabase.from('items').select('*')
+      return items
+    }
   }
 }
 </script>
