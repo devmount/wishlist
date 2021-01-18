cb137bc763a313628057446928f02656d6f754ef
Andreas Müller <mail@devmount.de>
2021-01-18 17:19:31 +0100

    ➕ implement add new list

--- src/views/Start.vue
+++ src/views/Start.vue
@@ -1,5 +1,47 @@
 <template>
   <div class="start">
     <h1>Wishlist</h1>
+    <section>
+      <h2>Neue Liste anlegen</h2>
+      <div>
+        <input type="text" v-model="title" placeholder="title" />
+        <input type="text" v-model="description" placeholder="description" />
+        <button @click="addList()">Liste hinzufügen</button>
+      </div>
+    </section>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'App',
+  data: () => ({
+    title: '',
+    description: '',
+  }),
+  methods: {
+    // store new item
+    async addList () {
+      const slugPublic = this.generateSlug(10)
+      const slugPrivate = this.generateSlug(16)
+      await this.$supabase.from('lists').insert({
+        'title': this.title,
+        'description': this.description,
+        'slug_public': slugPublic,
+        'slug_private': slugPrivate,
+      })
+      this.$router.push('/' + slugPublic + '/' + slugPrivate)
+    },
+    // get random slug string
+    generateSlug (length) {
+      const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+      let result = ''
+      for (let i = length; i>0; --i) result += chars[Math.round(Math.random()*(chars.length-1))]
+      return result
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+</style>
