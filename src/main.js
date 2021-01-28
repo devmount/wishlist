978f7cd001fc78783e62a5c6ba4c73893ec0b90e
Andreas Müller <mail@devmount.de>
2021-01-28 10:39:42 +0100

    🔧 improve config implementation with env file

--- src/main.js
+++ src/main.js
@@ -5,8 +5,8 @@ import router from './router'
 
 // Supabase
 import { createClient } from '@supabase/supabase-js'
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxMDg4MjU1MiwiZXhwIjoxOTI2NDU4NTUyfQ.FLgpeb22wLfHaZl6bQD6ztDG5UiTPTC9ympeN4bq0bc'
-const SUPABASE_URL = "https://ttohmduvoxaknazzfyre.supabase.co"
+const SUPABASE_KEY = process.env.VUE_APP_SB_KEY
+const SUPABASE_URL = process.env.VUE_APP_SB_URL
 
 const app = createApp(App)
 app.config.globalProperties.$supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
