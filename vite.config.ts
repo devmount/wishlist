f0c73870cf0ef09a501665993a91e890fb7ada94
Andreas Müller <mail@devmount.de>
2025-07-09 22:45:58 +0200

    🔨 Fix indentation

--- vite.config.ts
+++ vite.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
 		extensions: ['.ts', '.js', '.vue'],
 	},
 	test: {
-    globals: true,
-    environment: "jsdom",
-  },
+		globals: true,
+		environment: "jsdom",
+	},
 });
